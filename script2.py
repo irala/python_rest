51c213ffb0096455316dd484b03f58262336cac0
irala <nineirm@gmail.com>
2019-09-27 18:08:51 +0200

    changes no compile

--- script2.py
+++ script2.py
@@ -1,6 +1,10 @@
-
+print("script2.py's __name__: {}".format(__name__))
+print("script2.py's __package__: {}".format(__package__))
 def function_test():
   # Something
   print('esto es el fichero script2')
   return 1
 
+def function_sub():
+    print('esto es para el fichero sub')
+    return 1
---
+++ subfolder/script_sub.py
@@ -0,0 +1,6 @@
+print("script_sub.py's __name__: {}".format(__name__))
+print("script_sub.py's __package__: {}".format(__package__))
+from .. import script2 as
+
+script2.function_sub()
+
